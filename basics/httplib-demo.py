6122bfec8603f246ab839cc6119350d16961786f
gitTianxy <txy_learning@163.com>
2017-09-06 11:20:01 +0800

    add:basics

---
+++ basics/httplib-demo.py
@@ -0,0 +1,15 @@
+# encoding=utf-8
+"""
+httplib demo
+---------------
+TODO
+"""
+
+
+class HttpLibDemo:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    HttpLibDemo()
---
+++ basics/urllib-demo.py
@@ -0,0 +1,48 @@
+# encoding=utf-8
+"""
+urllib demo
+--------------
+"""
+import urllib
+import urllib2
+import socket
+
+
+class Urllib2Demo:
+    def __init__(self):
+        pass
+
+    def __encode_params(self, params):
+        return urllib.urlencode(params)
+
+    def open_url(self, url, data=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+        if data:
+            data = urllib.urlencode(data)
+        response = urllib2.urlopen(url, data, timeout)
+        print response.read()
+
+    def open_req(self, url, data=None, method=None, headers={}, proxy=False, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+        if data:
+            data = self.__encode_params(data)
+            if method and method == 'GET':
+                url += '?' + data
+                data = None
+        req = urllib2.Request(url, data, headers)
+        if method:
+            req.get_method = lambda: method
+        ''' TODO
+        if proxy:
+            proxy_handler = urllib2.ProxyHandler({
+                "http": 'http://localhost:1080',
+                "https": 'http://localhost:1080'
+            })
+            opener = urllib2.build_opener(proxy_handler)
+            urllib2.install_opener(opener)
+        '''
+        resp = urllib2.urlopen(req, timeout=timeout)
+        return resp.read()
+
+
+if __name__ == '__main__':
+    demo = Urllib2Demo()
+    print demo.open_req('http://www.baidu.com/')
